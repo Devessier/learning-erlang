2a86ca39aa20f4cef06689cddd972195aa273727
Devessier <baptiste@devessier.fr>
2021-02-05 00:58:47 +0100

    feat: add function to calculate the perimeter of the same shapes as area

--- modules-and-functions/geometry/geometry.erl
+++ modules-and-functions/geometry/geometry.erl
@@ -1,6 +1,7 @@
 -module(geometry).
 
 -export([area/1]).
+-export([perimeter/1]).
 
 -include_lib("eunit/include/eunit.hrl").
 
@@ -13,6 +14,15 @@ area({circle, Radius}) ->
 area({triangle, Base, Height}) ->
     (Base * Height) / 2.
 
+perimeter({rectangle, Width, Height}) ->
+    Width * 2 + Height * 2;
+perimeter({square, Side}) ->
+    Side * 4;
+perimeter({circle, Radius}) ->
+    2 * math:pi() * Radius;
+perimeter({triangle, Base, Height}) ->
+    Base + Height + math:sqrt(Base * Base + Height * Height).
+
 area_test_() ->
     [
         ?_assert(area({rectangle, 10, 5}) =:= 50),
@@ -20,4 +30,13 @@ area_test_() ->
         ?_assert(area({square, 10}) =:= 100),
         ?_assert(area({circle, 10}) =:= 314.1592653589793),
         ?_assert(area({triangle, 10, 5}) =:= 25.0)
+    ].
+
+perimeter_test_() ->
+    [
+        ?_assert(perimeter({rectangle, 10, 5}) =:= 30),
+        ?_assert(perimeter({rectangle, 0, 0}) =:= 0),
+        ?_assert(perimeter({square, 10}) =:= 40),
+        ?_assert(perimeter({circle, 10}) =:= 62.83185307179586),
+        ?_assert(perimeter({triangle, 10, 5}) =:= 26.180339887498949)
     ].
