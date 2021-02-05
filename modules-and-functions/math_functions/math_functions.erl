1072090e3ab858050f5c6a9daae9955d4f08649c
Devessier <baptiste@devessier.fr>
2021-02-05 02:06:26 +0100

    feat: create filter higher order function and use it with odd and even

--- modules-and-functions/math_functions/math_functions.erl
+++ modules-and-functions/math_functions/math_functions.erl
@@ -1,6 +1,6 @@
 -module(math_functions).
 
--export([even/1, odd/1]).
+-export([even/1, odd/1, filter/2]).
 
 -include_lib("eunit/include/eunit.hrl").
 
@@ -9,6 +9,14 @@ even(X) when is_integer(X) ->
 
 odd(X) when is_integer(X) ->
     abs(X rem 2) =:= 1.
+
+filter(F, [H|T]) when is_function(F) ->
+    case F(H) of
+        true -> [H|filter(F, T)];
+        false -> filter(F, T)
+    end;
+filter(_F, []) when is_function(_F) ->
+    [].
 
 even_test_() ->
     [
@@ -28,4 +36,10 @@ odd_test_() ->
         ?_assertNotMatch(true, odd(2)),
         ?_assertNotMatch(true, odd(-2)),
         ?_assertError(function_clause, odd(0.0))
+    ].
+
+filter_test_() ->
+    [
+        ?_assertMatch([-2, 0, 2, 4, 6], filter(fun even/1, [-2, -1, 0, 1, 2, 3, 4, 5, 6])),
+        ?_assertMatch([-1, 1, 3, 5], filter(fun odd/1, [-2, -1, 0, 1, 2, 3, 4, 5, 6]))
     ].
